aa1f32663a9f853969324d06d3f288e5e3254409
nvsblmike-szn <toluwani.alese@kibo.school>
2025-03-19 11:09:18 +0100

    remove node_modules

--- main.tf
+++ main.tf
@@ -217,7 +217,6 @@ resource "aws_instance" "ansible_controller" {
   }
 }
 
-
 resource "aws_instance" "jenkins_master" {
   ami                    = data.aws_ami.ubuntu.id 
   instance_type          = "t3.medium"
